b0c52f9c67df1a9699935f84dba80147c2d91698
Franklin Padilla <fpadilla@buk.cl>
2023-06-14 13:19:41 -0400

    code refactor

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
-
-
+const MIN_CELL = 0;
+const MAX_CELL = 255;
 
 type Action = '👉' | '👈' | '👆' | '👇' | '👊' | '🤜' | '🤛'
 type ActionTable = { 
   [key in Action]: () => void
 }
-
-
 
 export function translate(str: string): string {
   const memory: number[] = new Array(30000).fill(0);
@@ -14,45 +12,50 @@ export function translate(str: string): string {
   let output: string = '';
   let str_index: number = 0;
   const getCurrentAction: () => Action = () => str.slice(str_index, str_index+2) as Action;
+  const toNextAction: () => void = () => str_index += 2;
+  const topreviousAction: () => void = () => str_index -= 2;
+  const clamp: (num: number) => number = (num) => {
+    if (num < MIN_CELL) return MAX_CELL
+    if (num > MAX_CELL) return MIN_CELL
+    return num
+  };
+  const jumpToNextFist: () => void = () => {
+    let counter = 1;
+    while (counter > 0) {
+      toNextAction();
+      if (getCurrentAction() === '🤜') {
+        counter++;
+      } else if (getCurrentAction() === '🤛') {
+        counter--;
+      }
+    }
+  }
+  const jumpToPreviousFist: () => void = () => {
+    let counter = 1;
+    while (counter > 0) {
+      topreviousAction();
+      if (getCurrentAction() === '🤛') {
+        counter++;
+      } else if (getCurrentAction() === '🤜') {
+        counter--;
+      }
+    }
+  }
 
   const action_table: ActionTable = {
     '👉': ():void => { memory_pointer++ },
     '👈': ():void => { memory_pointer-- },
-    '👆': ():void => { memory[memory_pointer] = (memory[memory_pointer] < 255) ? memory[memory_pointer]+1 : 0},
-    '👇': ():void => { memory[memory_pointer] = (memory[memory_pointer] > 0) ? memory[memory_pointer]-1 : 255},
-    '👊': ():void => { output += String.fromCharCode(memory[memory_pointer])},
-    '🤜': ():void => {
-      if (memory[memory_pointer] === 0) {
-        let counter = 1;
-        while (counter > 0) {
-          str_index += 2;
-          if (getCurrentAction() === '🤜') {
-            counter++;
-          } else if (getCurrentAction() === '🤛') {
-            counter--;
-          }
-        }
-      }
-    },
-    '🤛': ():void => {
-      if (memory[memory_pointer] !== 0) {
-        let counter = 1;
-        while (counter > 0) {
-          str_index -= 2;
-          if (getCurrentAction() === '🤛') {
-            counter++;
-          } else if (getCurrentAction() === '🤜') {
-            counter--;
-          }
-        }
-      }
-    },
+    '👆': ():void => { memory[memory_pointer] = clamp(memory[memory_pointer]+1) },
+    '👇': ():void => { memory[memory_pointer] = clamp(memory[memory_pointer]-1) },
+    '👊': ():void => { output += String.fromCharCode(memory[memory_pointer]) },
+    '🤜': ():void => { if (memory[memory_pointer] === 0) jumpToNextFist() },
+    '🤛': ():void => { if (memory[memory_pointer] !== 0) jumpToPreviousFist(); },
   }
 
   while (str_index < str.length) {
     const action = getCurrentAction();
     action_table[action as Action](); 
-    str_index += 2;
+    toNextAction();
   }
 
   return output;
